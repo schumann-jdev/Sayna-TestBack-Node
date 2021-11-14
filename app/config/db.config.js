7e06a934c1af02a840b31405a1ba6230f5530603
Schumann Juda <judaaina@gmail.com>
2021-11-14 18:42:08 +0300

    finish

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     HOST: "localhost",
     PORT: 27017,
-    DB: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@schu.v1nkc.mongodb.net/${proccess.env.DB}?retryWrites=true&w=majority`
+    DB: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@schu.v1nkc.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
 }
